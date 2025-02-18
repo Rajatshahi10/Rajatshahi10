c6468891b70eb9101e8a6d927d0a571a45c85841
RAJAT SHAHI <rajatshahi102000@gmail.com>
2025-02-18 22:52:54 +0530

    Add files via upload

---
+++ inserttop.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[10], size, i, insert, position;
+
+    printf("Enter the size of the array: ");
+    scanf("%d", &size); // size of the array
+    printf("Enter the elements of the array: ");
+    for (i = 0; i < size; i++)
+    {
+        scanf("%d", &a[i]); // elements of the array
+    }
+
+    printf("Enter the element to be inserted: ");
+    scanf("%d", &insert); // element to be inserted
+
+    for (i = size - 1; i >= 0; i--)
+    {
+        a[i + 1] = a[i]; // shifting the elements to the right
+    }
+    a[0] = insert; // inserting the element at the start
+    size++;        // increasing the size of the array
+    printf("The array after insertion is: ");
+    for (i = 0; i < size; i++)
+    {
+        printf("%d ", a[i]); // printing the array after insertion
+    }
+
+    return 0;
+}
